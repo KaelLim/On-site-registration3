1f6c5d8878e101f196e1c558a000181de5d55781
KaelLim <kaelsohappy1@gmail.com>
2023-11-15 15:38:27 +0800

    Update check_duplicate.php

--- check_duplicate.php
+++ check_duplicate.php
@@ -1,21 +1,22 @@
 <?php
 
-$name = $_POST['name'];
 $phone = $_POST['phone'];
 $isDuplicate = false;
+$originalName = "";
 
 // 打開CSV檔案
 if (($handle = fopen("data.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         // 檢查是否有重複的資料
-        if ($data[0] == $name && $data[1] == $phone) {
+        if ($data[1] == $phone) {
             $isDuplicate = true;
+            $originalName = $data[0]; // 保存原始姓名
             break;
         }
     }
     fclose($handle);
 }
 
-echo json_encode(['isDuplicate' => $isDuplicate]);
+echo json_encode(['isDuplicate' => $isDuplicate, 'originalName' => $originalName]);
 
 ?>
