87c9cb4ed654bd5d14525e43d085f5aa13ed8944
KaelLim <kaelsohappy1@gmail.com>
2023-11-18 23:23:40 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,8 +3,8 @@
 $csvFile = 'data.csv';
 
 function randomSleep() {
-    $min = 100;  // 0.2秒
-    $max = 500;  // 1.3秒
+    $min = 200;  // 0.2秒
+    $max = 1300;  // 1.3秒
     $randomTime = rand($min, $max) * 1000;  // 從微秒轉為秒
     usleep($randomTime);
 }
@@ -25,44 +25,103 @@ function submitToCsv($data) {
         }
     }
 
-    // 寫入資料到該行
+    // 寫入資料到該行，暫時不包括 user_id
     $lineData = [
         $data['name'],
         $data['phone'],
-        $data['group'],  // 添加 group 資訊
+        $data['group'],
         $data['ldapAccount']
     ];
 
+    // 將現有數據寫入 CSV
     $fileContent[$firstEmptyRow] = implode(',', $lineData) . "\n";
     file_put_contents($csvFile, implode('', $fileContent));
 
-    // Send data to the API
     $apiUrl = 'https://community.tzuchi.org.tw/api/user';
-    // 省略 API 呼叫的其餘部分
+    $postData = [
+        "account" => "tc" . $data['phone'],
+        "ldap_id" => "tc" . $data['phone'],
+        "pass" => "tc" . $data['phone'],
+        "name" => $data['name'],
+        "email" => "tc" . $data['phone']. "@gmail.com",
+        "roles" => ["lv2_admin"]
+    ];
 
-    return ['result' => 'success', 'row' => $firstEmptyRow + 1];
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'accept: */*',
+        'Content-Type: application/json'
+    ]);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
+
+    $response = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        // Handle error
+        echo 'Curl error: ' . curl_error($ch);
+    } else {
+        $responseData = json_decode($response, true);
+   if (isset($responseData['user_id'])) {
+            // 更新 CSV 文件以包括 user_id
+            $lineData[] = $responseData['user_id'];
+            $fileContent[$firstEmptyRow] = implode(',', $lineData) . "\n";
+            file_put_contents($csvFile, implode('', $fileContent));
+
+            curl_close($ch);
+            return ['result' => 'success', 'row' => $firstEmptyRow + 1, 'user_id' => $responseData['user_id']];
+        }
+    }
+
+    curl_close($ch);
+    return ['result' => 'failure'];
 }
 
+
+
 function checkDuplicate($name, $phone) {
-    // 省略檢查重複的函數內容
+    global $csvFile;
+
+    $fileContent = file($csvFile);
+
+    foreach ($fileContent as $line) {
+        list($storedName, $storedPhone) = str_getcsv($line);
+        if ($storedName == $name && $storedPhone == $phone) {
+            return true;  // 找到重複的資料
+        }
+    }
+
+    return false;  // 沒有重複的資料
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $phone = $_POST['phone'];
-    $group = $_POST['group'];  // 獲取 group 資訊
     $ldapAccount = "tc" . $phone;
-    
+    $group = $_POST['group'];  // 直接獲取單一值
+
     if (!checkDuplicate($name, $phone)) {
         $response = submitToCsv([
             'name' => $name,
             'phone' => $phone,
-            'group' => $group,  // 將 group 資訊傳遞給函數
+            'group' => $group,
             'ldapAccount' => $ldapAccount
         ]);
 
-        // 省略剩餘部分
+        if ($response['result'] === 'success') {
+            // 這裡可以加入一些代碼，例如重定向到成功頁面或其他操作。
+            header("Location: index.php?success=1&row={$response['row']}");
+            exit;
+        } else {
+            // 處理錯誤情況
+        }
+    } else {
+        // 處理重複資料的情況
+        header("Location: index.php?duplicate=1");
+        echo "Name: " . $_POST['name'] . "<br>";
+        echo "Phone: " . $_POST['phone'] . "<br>";
+        exit;
     }
-    // 省略其他部分
 }
 ?>
